6866605c8a56806313f6950e857a622690f2d06e
oswalgopal <gploswal@gmail.com>
2021-03-16 16:02:56 +0530

    added road lines

--- js/custom/ShowMainRoad.js
+++ js/custom/ShowMainRoad.js
@@ -7,6 +7,7 @@ function showMainRoad(scene) {
     road.position.z = 200;
     road.position.y = 2;
     scene.add(road);
+    showRoadWhiteLines(scene);
     showLights(scene);
 }
 
@@ -24,5 +25,18 @@ function showLights(scene) {
         cylinder2.position.x = 400 + 100 * i;
         cylinder2.position.z = 300;
         scene.add( cylinder2);
+    }
+}
+
+function showRoadWhiteLines(scene){
+    var roadGeometry = new THREE.PlaneGeometry(50, 10, 1, 1);
+    roadGeometry.rotateX( - Math.PI / 2 );
+    const roadMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
+    for (let i = 1; i < 10; i++) {
+        const road = new THREE.Mesh(roadGeometry, roadMaterial);
+        road.position.x = 400 + 150 * i;
+        road.position.z = 200;
+        road.position.y = 3;
+        scene.add(road);
     }
 }
