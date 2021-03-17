1b7fd910c79eb2fbb0cd88f0fd910cbff3ca4b56
oswalgopal <gploswal@gmail.com>
2021-03-17 09:42:21 +0530

    added boundry wall

--- js/custom/ShowMainRoad.js
+++ js/custom/ShowMainRoad.js
@@ -1,14 +1,52 @@
 function showMainRoad(scene) {
+
+    // forward one
     var roadGeometry = new THREE.PlaneGeometry(1800, 200, 100, 100);
+    var roadGeometryWhite = new THREE.PlaneGeometry(1800, 250, 100, 100);
+    var roadGeometry1 = new THREE.PlaneGeometry(200, 3500, 100, 100);
+    var roadGeometryWhite1 = new THREE.PlaneGeometry(250, 3500, 100, 100);
+
     roadGeometry.rotateX( - Math.PI / 2 );
+    roadGeometryWhite.rotateX( - Math.PI / 2 );
+    roadGeometry1.rotateX( - Math.PI / 2 );
+    roadGeometryWhite1.rotateX( - Math.PI / 2 );
     const roadMaterial = new THREE.MeshBasicMaterial( {color: 0x4c5161, side: THREE.DoubleSide} );
+    const roadMaterialWhite = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
     const road = new THREE.Mesh(roadGeometry, roadMaterial);
-    road.position.x = 1400;
+    road.position.x = 1300;
     road.position.z = 200;
     road.position.y = 2;
     scene.add(road);
+    // const roadWhite = new THREE.Mesh(roadGeometryWhite, roadMaterialWhite);
+    // roadWhite.position.x = 1300;
+    // roadWhite.position.z = 200;
+    // road.position.y = 2;
+    // scene.add(roadWhite);
+    scene.add(road);
+    
     showRoadWhiteLines(scene);
     showLights(scene);
+
+    // backward one
+    const road1 = new THREE.Mesh(roadGeometry, roadMaterial);
+    road1.position.x = 2400;
+    road1.position.z = 200;
+    road1.position.y = 2;
+    scene.add(road1);
+
+    // left one
+    const road2 = new THREE.Mesh(roadGeometry1, roadMaterial);
+    road2.position.z = 0;
+    road2.position.x = 1800;
+    road2.position.y = 2;
+    scene.add(road2);
+
+    // right one
+    const road3 = new THREE.Mesh(roadGeometry1, roadMaterial);
+    road3.position.z = 600;
+    road3.position.x = 1800;
+    road3.position.y = 2;
+    scene.add(road3);
 }
 
 function showLights(scene) {
@@ -38,17 +76,48 @@ function showLights(scene) {
         tree2.position.y = 80;
         scene.add( tree2 );
     }
+
+    for (let i = 1; i < 10; i++) {
+        const cylinder = new THREE.Mesh( cylinderGeometry, cylinderMaterial );
+        cylinder.position.x = 2100 + 100 * i;
+        cylinder.position.z = 100;
+        scene.add( cylinder);
+        const cylinder2 = new THREE.Mesh( cylinderGeometry, cylinderMaterial );
+        cylinder2.position.x = 2100 + 100 * i;
+        cylinder2.position.z = 300;
+        scene.add( cylinder2);
+
+        const tree = new THREE.Mesh( treeGeometry, treeMaterial );
+        tree.position.x = 2100 + 100 * i;
+        tree.position.z = 100;
+        tree.position.y = 80;
+        scene.add( tree);
+        const tree2 = new THREE.Mesh( treeGeometry, treeMaterial );
+        tree2.position.x = 2100 + 100 * i;
+        tree2.position.z = 300;
+        tree2.position.y = 80;
+        scene.add( tree2 );
+    }
 }
 
 function showRoadWhiteLines(scene){
     var roadGeometry = new THREE.PlaneGeometry(50, 10, 1, 1);
+    var roadGeometry1 = new THREE.PlaneGeometry(10, 50, 1, 1);
     roadGeometry.rotateX( - Math.PI / 2 );
+    roadGeometry1.rotateX( - Math.PI / 2 );
     const roadMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
-    for (let i = 1; i < 13; i++) {
+    for (let i = 1; i < 20; i++) {
         const road = new THREE.Mesh(roadGeometry, roadMaterial);
         road.position.x = 400 + 150 * i;
         road.position.z = 200;
         road.position.y = 3;
         scene.add(road);
+    }
+    for (let i = 0 ; i < 25; i++){
+        const road1 = new THREE.Mesh(roadGeometry1, roadMaterial);
+        road1.position.x = 1800;
+        road1.position.z = -1800 + 150 * i;
+        road1.position.y = 3;
+        scene.add(road1);
     }
 }
---
+++ js/custom/boundryWall.js
@@ -0,0 +1,54 @@
+function showBoundryWall(scene) {
+    const cylinderGeometry = new THREE.CylinderGeometry( 3, 3, 50, 32 );
+    const cylinderGeometry1 = new THREE.CylinderGeometry( 2, 2, 50, 32 );
+    cylinderGeometry1.rotateX(Math.PI / 2);
+    const cylinderMaterial = new THREE.MeshBasicMaterial( {color: 0x6b7d8d} );    
+    const cylinderMaterial1 = new THREE.MeshBasicMaterial( {color: 0x414e5e} );    
+    for (let i = 1; i < 50; i++) {
+        const cylinder = new THREE.Mesh( cylinderGeometry, cylinderMaterial );
+        cylinder.position.x = 400;
+        cylinder.position.z = 250 + 50 * i;
+        scene.add( cylinder);
+        const cylinder2 = new THREE.Mesh( cylinderGeometry, cylinderMaterial );
+        cylinder2.position.x = 400;
+        cylinder2.position.z = 150 + (-50) * i;
+        scene.add( cylinder2);
+
+        // for center line
+        const cylinder3 = new THREE.Mesh( cylinderGeometry1, cylinderMaterial1 );
+        cylinder3.position.x = 400;
+        cylinder3.position.z = 120 + (-50) * i;
+        cylinder3.position.y = 10;
+        scene.add( cylinder3);
+
+        const cylinder4 = new THREE.Mesh( cylinderGeometry1, cylinderMaterial1 );
+        cylinder4.position.x = 400;
+        cylinder4.position.z = 120 + (-50) * i;
+        cylinder4.position.y = 15;
+        scene.add( cylinder4);
+
+        const cylinder5 = new THREE.Mesh( cylinderGeometry1, cylinderMaterial1 );
+        cylinder5.position.x = 400;
+        cylinder5.position.z = 120 + (-50) * i;
+        cylinder5.position.y = 20;
+        scene.add( cylinder5);
+
+        const cylinder6 = new THREE.Mesh( cylinderGeometry1, cylinderMaterial1 );
+        cylinder6.position.x = 400;
+        cylinder6.position.z = 275 + 50 * i;
+        cylinder6.position.y = 10;
+        scene.add( cylinder6);
+
+        const cylinder7 = new THREE.Mesh( cylinderGeometry1, cylinderMaterial1 );
+        cylinder7.position.x = 400;
+        cylinder7.position.z = 275 + 50 * i;
+        cylinder7.position.y = 15;
+        scene.add( cylinder7);
+
+        const cylinder8 = new THREE.Mesh( cylinderGeometry1, cylinderMaterial1 );
+        cylinder8.position.x = 400;
+        cylinder8.position.z = 275 + 50 * i;
+        cylinder8.position.y = 20;
+        scene.add( cylinder8);
+    }
+}
