d7c5c57f132e5d09d5f6fe8ae464830744ec0ae0
oswalgopal <gploswal@gmail.com>
2021-03-19 11:27:10 +0530

    added street light

--- js/custom/showSkills.js
+++ js/custom/showSkills.js
@@ -82,6 +82,7 @@ function showSkills(scene) {
     showSmallTrees(scene);
     showCementedGround(scene);
     showSkillsBanner(scene);
+    showStreetLamp(scene);
     // showEnd(scene);
 }
 
@@ -192,27 +193,107 @@ function showSmallTrees(scene) {
     const treeGeometry = new THREE.CylinderGeometry( 5, 10, 30, 32 );
     const treeMaterial = new THREE.MeshBasicMaterial( {color: 0x0c5c05} );
 
-    for (let i = 1; i < 36; i++) {
+    for (let i = 1; i < 18; i++) {
         const cylinder = new THREE.Mesh( cylinderGeometry, cylinderMaterial );
         cylinder.position.x = 1510;
-        cylinder.position.z = -150 + (-50) * i;
+        cylinder.position.z = -150 + (-100) * i;
         cylinder.position.y = 10;
         scene.add( cylinder);
         const cylinder2 = new THREE.Mesh( cylinderGeometry, cylinderMaterial );
         cylinder2.position.x = 2090;
-        cylinder2.position.z = -150 + (-50) * i;
+        cylinder2.position.z = -150 + (-100) * i;
         cylinder2.position.y = 10;
         scene.add( cylinder2);
 
         const tree = new THREE.Mesh( treeGeometry, treeMaterial );
         tree.position.x = 1510;
-        tree.position.z = -150 + -50 * i;
+        tree.position.z = -150 + -100 * i;
         tree.position.y = 35;
         scene.add( tree);
         const tree2 = new THREE.Mesh( treeGeometry, treeMaterial );
         tree2.position.x = 2090;
-        tree2.position.z = -150 + (-50) * i;
+        tree2.position.z = -150 + (-100) * i;
         tree2.position.y = 35;
         scene.add( tree2 );
+    }
+}
+
+function showStreetLamp(scene) {
+    const cylinderGeometry = new THREE.CylinderGeometry( 0.5, 2, 150, 32 );
+    const cylinderGeometry1 = new THREE.CylinderGeometry( 0.5, 0.5, 30, 32 );
+    cylinderGeometry1.rotateX(Math.PI / 2);
+    cylinderGeometry1.rotateY(Math.PI / 2);
+    cylinderGeometry1.rotateZ(Math.PI / 4);
+
+    const cylinderGeometry3 = new THREE.CylinderGeometry( 0.5, 0.5, 30, 32 );
+    cylinderGeometry3.rotateX(Math.PI / 2);
+    cylinderGeometry3.rotateY(Math.PI / 2);
+    cylinderGeometry3.rotateZ(Math.PI * 3 / 4);
+
+    const cylinderGeometry2 = new THREE.CylinderGeometry( 3, 3, 10, 32 );
+    cylinderGeometry2.rotateX(Math.PI / 2);
+    cylinderGeometry2.rotateY(Math.PI / 2);
+    
+    const cylinderMaterial = new THREE.MeshBasicMaterial( {color: 0xd7dcdf} );
+    // const cylinderMaterial1 = new THREE.MeshBasicMaterial( {color: 0xd7dcdf} );
+    // const treeGeometry = new THREE.CylinderGeometry( 5, 10, 30, 32 );
+    // const treeMaterial = new THREE.MeshBasicMaterial( {color: 0x0c5c05} );
+
+    for (let i = 1; i < 18; i++) {
+        const cylinder = new THREE.Mesh( cylinderGeometry, cylinderMaterial );
+        cylinder.position.x = 1510;
+        cylinder.position.z = -100 + (-100) * i;
+        cylinder.position.y = 10;
+        scene.add( cylinder);
+        const cylinder2 = new THREE.Mesh( cylinderGeometry, cylinderMaterial );
+        cylinder2.position.x = 2090;
+        cylinder2.position.z = -100 + (-100) * i;
+        cylinder2.position.y = 10;
+        scene.add( cylinder2);
+
+        const lamp = new THREE.Mesh( cylinderGeometry1, cylinderMaterial );
+        lamp.position.x = 1520;
+        lamp.position.z = -100 + (-100) * i;
+        lamp.position.y = 95;
+        scene.add( lamp);
+        const lamp1 = new THREE.Mesh( cylinderGeometry3, cylinderMaterial );
+        lamp1.position.x = 2080;
+        lamp1.position.z = -100 + (-100) * i;
+        lamp1.position.y = 95;
+        scene.add( lamp1);
+
+        const lampLight = new THREE.Mesh( cylinderGeometry2, cylinderMaterial );
+        lampLight.position.x = 1535;
+        lampLight.position.z = -100 + (-100) * i;
+        lampLight.position.y = 105;
+        scene.add( lampLight);
+        const lampLight1 = new THREE.Mesh( cylinderGeometry2, cylinderMaterial );
+        lampLight1.position.x = 2065;
+        lampLight1.position.z = -100 + (-100) * i;
+        lampLight1.position.y = 105;
+        scene.add( lampLight1);
+
+
+        // const cylinder3 = new THREE.Mesh( cylinderGeometry1, cylinderMaterial );
+        // cylinder3.position.x = 1510;
+        // cylinder3.position.z = -100 + (-100) * i;
+        // cylinder3.position.y = 40;
+        // scene.add( cylinder3);
+        // const cylinder4 = new THREE.Mesh( cylinderGeometry1, cylinderMaterial );
+        // cylinder4.position.x = 2090;
+        // cylinder4.position.z = -100 + (-100) * i;
+        // cylinder4.position.y = 40;
+        // scene.add( cylinder4);
+
+        // const tree = new THREE.Mesh( treeGeometry, treeMaterial );
+        // tree.position.x = 1510;
+        // tree.position.z = -150 + -50 * i;
+        // tree.position.y = 35;
+        // scene.add( tree);
+        // const tree2 = new THREE.Mesh( treeGeometry, treeMaterial );
+        // tree2.position.x = 2090;
+        // tree2.position.z = -150 + (-50) * i;
+        // tree2.position.y = 35;
+        // scene.add( tree2 );
     }
 }
